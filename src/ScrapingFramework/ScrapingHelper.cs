5179f5b687d9d860786521e356507b3d7b3879d4
Maxime Leboeuf <ml@maximeleboeuf.com>
2016-12-12 19:17:56 -0500

    Add TrimAll() to ScrapingHelper

--- src/ScrapingFramework/ScrapingHelper.cs
+++ src/ScrapingFramework/ScrapingHelper.cs
@@ -1,5 +1,6 @@
 ﻿using AngleSharp.Dom;
 using AngleSharp.Parser.Html;
+using System.Text.RegularExpressions;
 
 namespace ScrapingFramework
 {
@@ -13,6 +14,14 @@ namespace ScrapingFramework
         public static IDocument Parse(string html)
         {
             return _htmlParser.Parse(html);
+        }
+
+        /// <summary>
+        /// Trim whitespace at beginning, end and middle (multiple consecutive spaces) of a string.
+        /// </summary>
+        public static string TrimAll(string input)
+        {
+            return Regex.Replace(input, @"\s+", " ").Trim();
         }
     }
 }
