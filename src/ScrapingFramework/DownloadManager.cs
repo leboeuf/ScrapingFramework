3464febb43d489c637d0fe73b0e5f3a360751d54
Maxime Leboeuf <ml@maximeleboeuf.com>
2017-04-22 21:59:07 -0400

    Check for HTTP error before converting image to byte array

--- src/ScrapingFramework/DownloadManager.cs
+++ src/ScrapingFramework/DownloadManager.cs
@@ -1,5 +1,6 @@
 ﻿using ScrapingFramework.Interfaces;
 using System;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
@@ -32,12 +33,24 @@ namespace ScrapingFramework
 
         public async Task<string> DownloadImageToBase64(string url)
         {
+            const string b64scheme = "data:image/jpeg;base64,";
+            if (url.StartsWith(b64scheme))
+            {
+                return url.Replace(b64scheme, string.Empty);
+            }
+
             if (await _cachingProvider.HasKey(url))
             {
                 return await _cachingProvider.GetValue(url);
             }
 
-            var responseBytes = await _httpClient.GetByteArrayAsync(url);
+            var response = await _httpClient.GetAsync(url);
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return null;
+            }
+
+            var responseBytes = await response.Content.ReadAsByteArrayAsync();
             var base64 = Convert.ToBase64String(responseBytes);
 
             await _cachingProvider.CacheItem(url, base64);
